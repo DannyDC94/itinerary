ef18bd54b1201e1215b5667508b66cdd4b97653a
DannyDC <daniel.canarejo@easysoft.com.ec>
2023-10-13 00:36:46 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CdkDrag, CdkDropList, CdkDropListGroup} from "@angular/cdk/drag-drop";
+import {NgFor} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CdkDropListGroup,
+    CdkDropList,
+    NgFor,
+    CdkDrag
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,99 @@
+import { Component } from '@angular/core';
+import {NgFor} from '@angular/common';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+
+export class TaskComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  list: any[] = [];
+  tasks: any[] = [
+    {
+      id: 1,
+      title: 'Subida al cerro',
+      type: "ACTIVITY",
+      startDate: '2022-01-01 00:00:00',
+      endDate: '2022-01-02 00:00:00',
+      status: 'IN_PROGRESS'
+    },
+    {
+      id: 2,
+      title: 'Fiesta de espuma',
+      type: "PARTY",
+      startDate: '2022-01-01 00:00:00',
+      endDate: '2022-01-03 00:00:00',
+      status: 'DONE'
+    },
+    {
+      id: 3,
+      title: 'Desayuno',
+      type: "FOOD",
+      startDate: null,
+      endDate: null,
+      status: null
+    },
+    {
+      id: 4,
+      title: 'Fiesta de vaga',
+      type: "PARTY",
+      startDate: '2022-01-03 00:00:00',
+      endDate: '2022-01-04 00:00:00',
+      status: 'DONE'
+    },
+    {
+      id: 5,
+      title: 'Almuerzo',
+      type: "FOOD",
+      startDate: null,
+      endDate: null,
+      status: null
+    },
+  ];
+  constructor() {}
+  ngOnInit(): void {
+    this.buildListTasks();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(this.list);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+  buildListTasks() {
+    this.tasks.map((t, idx) => {
+      const dateL = t.startDate;
+      console.log(dateL);
+      const taskTemp = {
+        id: idx,
+        title: dateL,
+        task: [t]
+      }
+      const id = this.list.findIndex(li => li.title === t.startDate);
+      if (id > -1)
+        this.list[id].task.push(t);
+      else
+        this.list.push(taskTemp);
+    })
+    console.log(this.list);
+  }
+}
