ff041eb51b331c1cfd6b9a6e71ed022550080fa9
DannyDC <daniel.canarejo@easysoft.com.ec>
2023-10-13 09:48:15 -0500

    Add datepicker

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -17,7 +17,7 @@ import {
 
 export class TaskComponent {
   @ViewChild('childModal', { static: false }) childModal?: ModalDirective;
-  formData: { name: string, email: string, message: string, dateFrom:string } = { name: '', email: '', message: '', dateFrom: '' };
+  formData: { name: string, email: string, message: string, dateFrom:Date } = { name: '', email: '', message: '', dateFrom: new Date()};
   todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
   done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
   list: any[] = [];
@@ -110,5 +110,9 @@ export class TaskComponent {
 
   hideChildModal(): void {
     this.childModal?.hide();
+  }
+
+  onDateChanged(event: Date) {
+    this.formData.dateFrom = event;
   }
 }
